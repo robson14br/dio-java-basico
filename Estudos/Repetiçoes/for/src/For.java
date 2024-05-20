03d5aacdaa6a54dbc8e0b9edd9b166026b3e13fe
Robson <robson14br@outlook.com>
2024-05-20 15:24:23 -0300

    Repetiçoes

---
+++ Estudos/Repetiçoes/for/src/For.java
@@ -0,0 +1,10 @@
+public class For {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.println("Contando carneirinhos " + carneirinhos);
+        }
+        System.out.println("Joaozinho dormiu!");
+    }
+    
+} 
+        
